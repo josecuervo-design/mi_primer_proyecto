612f1689355eccb872df3c1d9d864137b96448c5
jose_cuervo <jose2008cuervo@gmail.com>
2026-02-23 17:57:33 -0500

    creacion de ejercicios

---
+++ Ejercios.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Ejercios {
+    public static void main(String[] args) {
+        System.out.println("Hola");
+
+       Scanner n = new Scanner(System.in);
+       System.out.println("Ingrese un numero");
+       int num;
+       num=n.nextInt();
+       System.out.println("tu numero es el :"+num);
+
+
+    }
+}
